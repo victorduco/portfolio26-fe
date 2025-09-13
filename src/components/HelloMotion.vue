ce57b45d6bc782de54b470aa02b965005ce63013
Victor Diukov <mail@victorduco.com>
2025-09-13 11:03:08 -0700

    Initial commit: Vue + Motion One Hello World

---
+++ src/components/HelloMotion.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 ref="motionEl">Hello World с Motion One и Vue!</h1>
+    <button @click="animate">Animate</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { animate } from 'motion'
+
+const motionEl = ref(null)
+
+function animate() {
+  if (motionEl.value) {
+    animate(motionEl.value, { scale: [1, 1.5, 1], rotate: [0, 360, 0] }, { duration: 1.2 })
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  transition: color 0.3s;
+  color: #42b983;
+}
+button {
+  margin-top: 16px;
+  padding: 8px 16px;
+  font-size: 1rem;
+}
+</style>
