667359c7ed871ba83667451768f5b74e4d2f6cb0
Victor Diukov <mail@victorduco.com>
2025-10-01 13:38:56 -0700

    feat: uncomment Keypad component and comment out BgToSvg and Intro components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { provide, ref } from "vue";
 import Intro from "./components/intro/Intro.vue";
-// import Keypad from "./components/keypad/Keypad.vue";
+import Keypad from "./components/keypad/Keypad.vue";
 import BgToSvg from "./components/bg-to-svg/BgToSvg.vue";
 
 const sourceSelector = ref("intro-text-export-node");
 </script>
 
 <template>
-  <BgToSvg :source-selector="sourceSelector" :render-delay="100" />
-  <Intro />
-  <!-- <Keypad /> -->
+  <!-- <BgToSvg :source-selector="sourceSelector" :render-delay="100" />
+  <Intro /> -->
+  <Keypad />
 </template>
