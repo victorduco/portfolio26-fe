6faa0fcb3425ce46620bff2ec71cb5432b3b3237
Victor Diukov <mail@victorduco.com>
2025-10-05 20:55:38 -0700

    feat: remove unnecessary glass effect parameters for simplified configuration

--- src/components/keypad/glassEffectConfig.js
+++ src/components/keypad/glassEffectConfig.js
@@ -1,9 +1,4 @@
 export const glassEffectConfig = {
   displacementMap: "/src/assets/distmaps/100-1-d.png",
   displacementScale: 300,
-  glassBlur: 15,
-  glassSaturation: 215,
-  glassBrightness: 146,
-  glassContrast: 103,
-  highlightReflection: 0.23,
 };
