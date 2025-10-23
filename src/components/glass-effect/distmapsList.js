89b8b0b31df9325b48ae5b23e3da80dd23b76cf7
Victor Diukov <mail@victorduco.com>
2025-10-23 09:25:18 -0700

    refactor: remove unused glass effect components and related files to clean up the codebase

--- src/components/glass-effect/distmapsList.js
+++
@@ -1,4 +0,0 @@
-// List of available displacement maps
-export const availableDistmaps = [
-  { value: "/distmaps/100-1-d.png", label: "100-1-d (Production)" },
-];
--- src/pages/main-page/intro/index.js
+++
@@ -1,1 +0,0 @@
-export { default as IntroRectangle } from "./IntroRectangle.vue";
